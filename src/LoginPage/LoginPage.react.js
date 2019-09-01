018e495ad7c954bdbeea9616727b3c22d42674ec
lakshya03 <lakshyajain03@gmail.com>
2019-09-01 21:43:29 +0530

    initial commit

---
+++ src/LoginPage/LoginPage.react.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        };
+    }
+
+    render() {
+        return (
+            <div >
+              
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
